796d8bab651a62e045cc9e27e95b27bd0f40fa87
psl <pawel.slowik.dev@gmail.com>
2017-03-04 11:30:53 +0100

    Added monochrome option for better output formatting in Cucumber

--- cucumber/src/test/java/pl/com/psl/testing/cucumber/TestRunner.java
+++ cucumber/src/test/java/pl/com/psl/testing/cucumber/TestRunner.java
@@ -8,6 +8,6 @@ import org.junit.runner.RunWith;
  * Created by psl on 03.03.17.
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "classpath:features/", format = {"pretty", "html:target/cucumber-reports"})
+@CucumberOptions(features = "classpath:features/", format = {"pretty", "html:target/cucumber-reports"}, monochrome = true)
 public class TestRunner {
 }
